b754ec1536a98d2252b495f5fc3445f6ed3f5aa4
Lukas <lukasdevarga@gmail.com>
2019-04-28 18:23:19 +0200

    apiKeys eliminated from git

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -2,15 +2,16 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 import 'firebase/storage';
+import data from './apiKey.json';
 // Initialize Firebase
 const config = {
-  apiKey: "AIzaSyAISWAeSwgffrXmXR1ekSBo0NytlwoOsBI",
-  authDomain: "adifox-37688.firebaseapp.com",
-  databaseURL: "https://adifox-37688.firebaseio.com",
-  projectId: "adifox-37688",
-  storageBucket: "adifox-37688.appspot.com",
-  messagingSenderId: "155182142796"
-};
+  apiKey: data.key.apiKey,
+  authDomain: data.key.authDomain,
+  databaseURL: data.key.authDomain,
+  projectId: data.key.projectId,
+  storageBucket: data.key.storageBucket,
+  messagingSenderId: data.key.messagingSenderId
+}
 
 firebase.initializeApp(config);
 firebase.firestore();
